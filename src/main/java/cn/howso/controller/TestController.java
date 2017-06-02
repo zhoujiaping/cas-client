b204c100653307902678dfba156bc47d7dba4a30
zhoujiaping <421211679@qq.com>
2017-06-02 18:40:35 +0800

    redis

--- src/main/java/cn/howso/controller/TestController.java
+++ src/main/java/cn/howso/controller/TestController.java
@@ -1,5 +1,8 @@
 package cn.howso.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.SecurityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
     @RequestMapping("test")
     @ResponseBody
-    public Object test(){
+    public Object test(HttpServletRequest req){
+        Object obj = SecurityUtils.getSubject().getSession().getAttribute("my-key");
+        System.out.println(obj);
+        SecurityUtils.getSubject().getSession().setAttribute("my-key", req.getContextPath());
         return "test";
     }
 }
